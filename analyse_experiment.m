c34f08953cfabb02aa37f7b568135f5d3b12141a
are-mj <59837393+are-mj@users.noreply.github.com>
2024-10-26 17:53:54 +0200

    Changed table names to Trip, Tzip

--- analyse_experiment.m
+++ analyse_experiment.m
@@ -1,11 +1,11 @@
-function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,par,plotting)
+function [Trip,Tzip,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,par,plotting)
 % Find rips and zips data in protein stretching experiemnt records
 % Input:
 %   file:     data file from optical tweezer instrument
 %   par:      parameter struct.  Default: parameter_struct
 %   plotting: 1 for force vs time plot.  Default: no plot
 %  Output:
-%  Tp, Tr:      Matlab tables of properties of rips (Tp) and zips (Tr)
+%  Trip, Tzip:  Matlab tables of properties of rips and zips
 %  pull, relax: struct arrays with detailed data for pulling and relax
 %               traces with an identified rip or zip
 %  t,f,x,T:     arrays of time, force, trap position and temperature for
@@ -25,7 +25,7 @@ function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,
   else
     filename = fullfile(datafolder,file);
     if ~isfile(filename)
-      error("File %s is not found",filename);
+      error("File %p is not found",filename);
     end
   end
   Tlist = NaN;
@@ -33,6 +33,10 @@ function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,
     Tlist = par.Tlist;
   end
   [t,f,x,T] = read_experiment_file(filename,Tlist);
+  if numel(t)<10
+    waitfor(msgbox(sprintf('%s contains no useful data',filename)));
+    error('Bad eperiment file')
+  end
   
   % Eliminate obviously faulty records
   bad = isnan(x) | isnan(f) | isnan(t);
@@ -58,14 +62,14 @@ function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,
   x(1:valleypos(1)) = x(1:valleypos(1))-x(valleypos(1));
   for i = 2:numel(valleypos)
     rng = valleypos(i-1):valleypos(i);
-    p = polyfit([rng(1),rng(end)],x([rng(1),rng(end)]),1);  
-    x(rng(1:end-1)) = x(rng(1:end-1))-polyval(p,rng(1:end-1))';
+    px = polyfit([rng(1),rng(end)],x([rng(1),rng(end)]),1);  
+    x(rng(1:end-1)) = x(rng(1:end-1))-polyval(px,rng(1:end-1))';
   end  
 
   pull = [];  % Array of pulling trace structs
   relax = []; % Array of relaxing trace structs
-  Tp = [];    % Pulling results table
-  Tr = [];    % Relaxing results table
+  Trip = [];    % Pulling results table
+  Tzip = [];    % Relaxing results table
   if plotting
     plot(t(peakpos),f(peakpos),'.k',t(valleypos),f(valleypos),'.k');
   end
@@ -76,20 +80,18 @@ function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,
     if numel(rng)<50  % not a real trace
       continue
     end
-    s.t = t(rng);
-    s.f = f(rng);
-    s.x = x(rng);
-    s.T = T(rng);
-    s.file = filename;
-    s = singlerip_finder(s,par);
-    s.pullingspeed = median(diff(s.x)./diff(s.t));
+    % Pulling trace struct p
+    p.t = t(rng);
+    p.f = f(rng);
+    p.x = x(rng);
+    p.T = T(rng);
+    p.file = filename;
+    p = singlerip_finder(p,par);
+    p.pullingspeed = median(diff(p.x)./diff(p.t));
     % Rips with very low deltax are not likely to be real:
-    if ~isempty(s.force) && s.deltax > 5
-      if plotting
-        plot(s.time,s.force,'*r');
-      end
-      pull = [pull;s];
-      Tp = [Tp;create_table(s)];
+    if ~isempty(p.force) && p.deltax > 5
+      pull = [pull;p];
+      Trip = [Trip;create_table(p)];
     end
   end
   for i = 1:nvalleys-1
@@ -97,6 +99,7 @@ function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,
     if numel(rng)<50
       continue  % not a real trace
     end
+    % Relaxing trace struct r
     r.t = t(rng);
     r.f = f(rng);
     r.x = x(rng);
@@ -106,11 +109,16 @@ function [Tp,Tr,pull,relax,t,f,x,T,peakpos,valleypos] = analyse_experiment(file,
     r.pullingspeed = abs(median(diff(r.x)./diff(r.t)));
     r.file = filename;
     if ~isempty(r.force) && r.force < f(peakpos(i))*0.5 && r.deltax < -2
-      if plotting
-        plot(r.time,r.force,'ok',MarkerFaceColor='w');
-      end
       relax = [relax;r];
-      Tr = [Tr;create_table(r)];
+      Tzip = [Tzip;create_table(r)];
     end 
+  end
+  if plotting
+    if ~isempty(Trip)
+      plot(Trip.Time,Trip.Force,'*r')
+    end
+    if ~isempty(Tzip)
+      plot(Tzip.Time,Tzip.Force,'ok',MarkerFaceColor='w');
+    end
   end
 end
--- analyse_many.m
+++ analyse_many.m
@@ -1,5 +1,5 @@
-function [TP,TR] = analyse_many(files,plotting)
-% Anslyse list of files and concatenate results tables in TP and TR
+function [TRIP,TZIP] = analyse_many(files,plotting)
+% Anslyse list of files and concatenate results tables in TRIP and TZIP
 % Input:
 %   files: list of file names.  
 %          Either:  Full path and file name, starting with 'C:\'
@@ -11,17 +11,17 @@ function [TP,TR] = analyse_many(files,plotting)
 if nargin < 2
   plotting = 0;
 end
-TP = [];
-TR = [];
+TRIP = [];
+TZIP = [];
 for i = 1:numel(files)
   if plotting
     figure(i);
     clf;
   end
-  [Tp,Tr] = analyse_experiment(files(i),parameter_struct,plotting);
-  TP =[TP;Tp];
-  TR = [TR;Tr];
-  fprintf('Rips: %4d, Zips: %4d Filename: %s\n',height(Tp),height(Tr),files(i));
+  [Trip,Tzip] = analyse_experiment(files(i),parameter_struct,plotting);
+  TRIP =[TRIP;Trip];
+  TZIP = [TZIP;Tzip];
+  fprintf('Rips: %4d, Zips: %4d Filename: %s\n',height(Trip),height(Tzip),files(i));
   if plotting
     drawnow;
   end
