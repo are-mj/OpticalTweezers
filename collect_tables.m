92f1b8d90a28008b80695fac39d7327226e193e1
are-mj <59837393+are-mj@users.noreply.github.com>
2024-10-26 18:06:33 +0200

    New file

---
+++ collect_tables.m
@@ -0,0 +1,32 @@
+function [TRIP,TZIP] = collect_tables(filelist,Outputfolder)
+% Creates total rip and zip results tables for the files in filelist
+% Uses tables saved from the RipAnalysis app if they exist
+% Otherwise calcuates tables using function analyse_experiment
+% Input: 
+%   filelist: Strnng array of file names
+%   Outputfolder:  Folder used by the RipAnalysis app. 
+%      Default: Uses current Outputfolder      
+%
+
+  if nargin < 2
+    load("RipAnalysis_settings.mat","appsettings")
+    Outputfolder = appsettings.Outputfolder;
+  end
+  TRIP = [];
+  TZIP = [];
+  for i = 1:numel(filelist)
+    matfile = fullfile(Outputfolder,strrep(filelist(i),".txt",".mat"));
+    if exist(matfile,"file")
+      load(matfile,"Trip","Tzip");
+      fprintf("Collects tables from the Outputfolder for %s\n",filelist(i));
+    else
+      [Trip,Tzip] = analyse_experiment(filelist(i));
+      fprintf("Calculates tables for %s\n",filelist(i));
+    end
+    if ~isempty(Trip)
+      TRIP = [TRIP;Trip];
+    end
+    if ~isempty(Tzip)
+      TZIP = [TZIP;Tzip];
+    end
+  end
