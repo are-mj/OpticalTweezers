22f79b68d6d01eafe62b131e12b636466b8068ef
are-mj <59837393+are-mj@users.noreply.github.com>
2024-10-28 16:23:19 +0100

    Imroved help text

--- datafolder.m
+++ datafolder.m
@@ -1,13 +1,11 @@
 function folder = datafolder
-% Returns the folder where the optical tweezer experiment measurements
-% are stored.  
-%
-% Example: To plot data from file kA.txt in folder 02042022:
-%  plotfile(fullfile(datafolder,'02042022','kA.txt'));
-% or: 
-%  plotfile(fullfile(datafolder,'02042022/kA.txt'));
+% Returns the full path folder where the optical tweezer experiment 
+% measurements are stored. 
+% Example (windows)
+%  folder = "C:\users\are\data\tweezers"
+
+% This work for me on both my PCs that use diferent user names
   home = [getenv('HOMEDRIVE') getenv('HOMEPATH')];
   localfolder = '\OneDrive\Data\Chile';
-  % localfolder = '\Dropbox\Projects\Chile\Karina_New\Clean_files';
   folder = [home,localfolder];
 end
