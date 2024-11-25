44abde4d6db988ee0931aedd6f5471903e7172a0
are-mj <59837393+are-mj@users.noreply.github.com>
2024-11-25 15:22:22 +0100

    Restored code lines deleted by mistake

--- analyse_many.m
+++ analyse_many.m
@@ -3,7 +3,24 @@ function [TRIP,TZIP] = analyse_many(files,plotting,par)
 % Input:
 %   files: list of file names.  
 %          Either:  Full path and file name, starting with 'C:\'
-%          or:      Short name, such that fullfile(datafolder,file) g
+%          or:      Short name, such that fullfile(datafolder,file) gives
+%                   complete path and file
+%   plotting:  1 - plot results
+%              0 or absent:  Do not plot
+
+if nargin < 3
+  par = params;
+end
+if nargin < 2
+  plotting = 0;
+end
+TRIP = [];
+TZIP = [];
+for i = 1:numel(files)
+  if plotting
+    figure(i);
+    clf;
+  end
   [Trip,Tzip] = analyse_experiment(files(i),plotting,par);
   TRIP =[TRIP;Trip];
   TZIP = [TZIP;Tzip];
