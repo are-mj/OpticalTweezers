eb224b5bc537f241c7605487c25f6557e506ad52
are <are@Ares_Asus>
2025-01-06 17:23:51 +0100

    Added minpeak_slope, which earlier was hardcoded in singlerip_finder

--- params.m
+++ params.m
@@ -10,6 +10,7 @@ function par = params
   par.minfitfraction = 0.05;  
   par.maxfitfraction = 0.15;  
   par.supportlength = 5; % number of points used for the movingslope window
+  par.minpeak_slope = 0.003; % Minimum slope peak height in singlerip_finder
   par.ripsteps = 1;      % Number of timesteps from rip start to steepest force change
   par.min_fstep = 0.4;   % Minimum reduction in force at an unfoding (pN)
   par.maxrips = 1;       % Maximum number of rips/zips accepted per trace
@@ -22,7 +23,7 @@ function par = params
 % Extra heating table. 
 % Row 1: heater setting, from digits 2 and 3 in status column.
 % Row 2: Corresponding temperature rise over mean COM file value
-  par.Tlist = [0 2 4 6 8 10 14 12 16 20 24 31; ...
+  par.Tlist = [0 2 4 6 8 10 12 14 16 20 24 31; ...
   0 3.07 6.96 10.78 13.75 16.92 20.14 22.92 25.10 28.01 30.77 34.83];
 % Ad-hoc change to handle IR laser (09=
   % par.Tlist = [0 2 4 6 8 9 10 14 12 16 20 24 31; ...
