ba355aff7fd20e35bf9f6f6b951d33584f4bf0df
are <are@Ares_Asus>
2025-02-04 10:40:22 +0100

    Changed input from flile list to results table, for greater flexibility

--- sortfibers.m
+++ sortfibers.m
@@ -1,8 +1,13 @@
-function fiberlist = sortfibers(folders)
-% Fibers in folders sorted by number of rips and zips
-  files = textfile_names(folders);
-  [TP,TR] = analyse_many(files);
-  fiber_array = arrayfun(@(x) fiber(x),[TP.Filename;TR.Filename]);
+function fiberlist = sortfibers(T)
+% Fibers in optical  tweezers result tables ordered by number of rips/zips
+% Input: T optical twezzers result table from collect_results/analyse_many
+% Example: 
+  % folders = ["20231128","20231129","20250111"];
+  % files = textfile_names(folders);
+  % [TP,TR] = analyse_many(files);
+  % fiberlist = sortfibers([TP;TR])
+  
+  fiber_array = arrayfun(@(x) fiber(x),T.Filename);
   fibers =unique(fiber_array);
   counts = zeros(length(fibers),1);
   for i = 1:length(fibers)
